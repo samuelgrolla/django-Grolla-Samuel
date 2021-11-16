eff9a49c3cc0f89244da8b783336c62766e1a360
samuelgrolla <92153338+samuelgrolla@users.noreply.github.com>
2021-11-16 08:53:18 +0100

    bugfix

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("prima_app.urls",namespace="prima_app"))
-    path('seconda_app/',include("seconda_app.urls", namespace="seconda_app"))
+    path('',include("prima_app.urls",namespace="prima_app")),
+    path('seconda_app/',include("seconda_app.urls", namespace="seconda_app")),
 ]
